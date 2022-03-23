259be950e07f1885a7947058ebadff091e2f2d7c
Anton Patsev <patsev.anton@gmail.com>
2022-03-23 19:57:49 +0600

    Install Plugins on behalf of jenkins user "admin"

--- 05-provisioner-jenkins-by-ansible/jenkins_install.sh
+++ 05-provisioner-jenkins-by-ansible/jenkins_install.sh
@@ -6,6 +6,7 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
+ansible-galaxy install robertdebock.java
 ansible-galaxy install robertdebock.jenkins
 ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
