39a945ae3aed8d5d1d3cd2e90b98b394b9388217
Anton Patsev <patsev.anton@gmail.com>
2022-03-23 14:46:42 +0600

    remove jenkins_external_url

--- 05-provisioner-jenkins-by-ansible/variable.tf
+++ 05-provisioner-jenkins-by-ansible/variable.tf
@@ -49,11 +49,6 @@ variable "hostname" {
   description = "hostname"
 }
 
-variable "jenkins_external_url" {
-  type        = string
-  description = "jenkins_external_url"
-}
-
 variable "domain" {
   type        = string
   description = "domain"
